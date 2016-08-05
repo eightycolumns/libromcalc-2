b1e8bba56a7f9f4f3c036c9f30f2c82955fe9bce
Drew Keller <drew@eightycolumns.com>
2016-08-05 19:20:59 -0400

    Test IX - IV = V

--- test/romcalc-test.c
+++ test/romcalc-test.c
@@ -128,6 +128,12 @@ START_TEST(X_minus_I_is_IX) {
   ck_assert_str_eq(expected, actual);
 } END_TEST
 
+START_TEST(IX_minus_IV_is_V) {
+  char expected[] = "V";
+  subtract(actual, "IX", "IV");
+  ck_assert_str_eq(expected, actual);
+} END_TEST
+
 int main(void) {
   TCase *success = tcase_create("Success");
   tcase_add_test(success, I_is_a_valid_operand);
@@ -160,6 +166,7 @@ int main(void) {
 
   TCase *subtraction = tcase_create("Subtraction");
   tcase_add_test(subtraction, X_minus_I_is_IX);
+  tcase_add_test(subtraction, IX_minus_IV_is_V);
 
   Suite *operations = suite_create("Operations");
   suite_add_tcase(operations, addition);
