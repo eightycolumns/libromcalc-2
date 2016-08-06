12bf01e4f60d9f17f1e96f03ab17a21c7cbb20aa
Drew Keller <drew@eightycolumns.com>
2016-08-06 17:55:48 -0400

    Reformat numerals array

--- src/numeral-conversion.c
+++ src/numeral-conversion.c
@@ -11,19 +11,13 @@ typedef struct {
 } Numeral;
 
 static Numeral numerals[] = {
-  {"M", 1000},
-  {"CM", 900},
-  {"D", 500},
-  {"CD", 400},
-  {"C", 100},
-  {"XC", 90},
-  {"L", 50},
-  {"XL", 40},
-  {"X", 10},
-  {"IX", 9},
-  {"V", 5},
-  {"IV", 4},
-  {"I", 1}
+  {"M", 1000}, {"CM", 900},
+  {"D",  500}, {"CD", 400},
+  {"C",  100}, {"XC",  90},
+  {"L",   50}, {"XL",  40},
+  {"X",   10}, {"IX",   9},
+  {"V",    5}, {"IV",   4},
+  {"I",    1}
 };
 
 static size_t numeral_count = sizeof numerals / sizeof numerals[0];
