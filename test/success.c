1416feaac614826801f03047510162e6815f6bcd
Drew Keller <drew@eightycolumns.com>
2016-08-06 17:03:30 -0400

    Accept mmmcmxcix as a valid operand

--- test/success.c
+++ test/success.c
@@ -151,6 +151,12 @@ START_TEST(MMMCMXCIX_is_a_valid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(mmmcmxcix_is_a_valid_operand) {
+  int expected = ROMCALC_SUCCESS;
+  int actual = subtract(difference, "mmmcmxcix", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *success(void) {
   TCase *success = tcase_create("Success");
   tcase_add_test(success, I_is_a_valid_operand);
@@ -177,6 +183,7 @@ TCase *success(void) {
   tcase_add_test(success, MMMDCCCLXXXVIII_is_a_valid_operand);
   tcase_add_test(success, mmmdccclxxxviii_is_a_valid_operand);
   tcase_add_test(success, MMMCMXCIX_is_a_valid_operand);
+  tcase_add_test(success, mmmcmxcix_is_a_valid_operand);
 
   return success;
 }
