771b3773eb8cd462bd22ef98326dcfa6eaa954b9
Drew Keller <drew@eightycolumns.com>
2016-08-06 18:04:46 -0400

    Check for invalid addition results

--- test/error-handling.c
+++ test/error-handling.c
@@ -5,12 +5,14 @@
 #include "test/success.h"
 #include "test/null-pointer-error.h"
 #include "test/invalid-operand-error.h"
+#include "test/invalid-result-error.h"
 
 Suite *error_handling(void) {
   Suite *error_handling = suite_create("Error Handling");
   suite_add_tcase(error_handling, success());
   suite_add_tcase(error_handling, null_pointer_error());
   suite_add_tcase(error_handling, invalid_operand_error());
+  suite_add_tcase(error_handling, invalid_result_error());
 
   return error_handling;
 }
