669c0531e06fd9b04cae27aaf9c52c6c650d8242
Drew Keller <drew@eightycolumns.com>
2016-08-05 20:16:34 -0400

    Extract addition.{c,h} from romcalc-test.c

--- test/romcalc-test.c
+++ test/romcalc-test.c
@@ -6,44 +6,9 @@
 #include "test/success.h"
 #include "test/null-pointer-error.h"
 #include "test/invalid-operand-error.h"
+#include "test/addition.h"
 
 static char actual[ROMCALC_MAX_LENGTH];
-
-START_TEST(I_plus_I_is_II) {
-  char expected[] = "II";
-  add(actual, "I", "I");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
-
-START_TEST(II_plus_I_is_III) {
-  char expected[] = "III";
-  add(actual, "II", "I");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
-
-START_TEST(III_plus_I_is_IV) {
-  char expected[] = "IV";
-  add(actual, "III", "I");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
-
-START_TEST(IV_plus_I_is_V) {
-  char expected[] = "V";
-  add(actual, "IV", "I");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
-
-START_TEST(V_plus_IV_is_IX) {
-  char expected[] = "IX";
-  add(actual, "V", "IV");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
-
-START_TEST(IX_plus_I_is_X) {
-  char expected[] = "X";
-  add(actual, "IX", "I");
-  ck_assert_str_eq(expected, actual);
-} END_TEST
 
 START_TEST(X_minus_I_is_IX) {
   char expected[] = "IX";
@@ -75,7 +40,6 @@ START_TEST(II_minus_I_is_I) {
   ck_assert_str_eq(expected, actual);
 } END_TEST
 
-static TCase *addition(void);
 static TCase *subtraction(void);
 
 int main(void) {
@@ -96,18 +60,6 @@ int main(void) {
   srunner_free(srunner);
 
   return (ntests_failed > 0) ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
-static TCase *addition(void) {
-  TCase *addition = tcase_create("Addition");
-  tcase_add_test(addition, I_plus_I_is_II);
-  tcase_add_test(addition, II_plus_I_is_III);
-  tcase_add_test(addition, III_plus_I_is_IV);
-  tcase_add_test(addition, IV_plus_I_is_V);
-  tcase_add_test(addition, V_plus_IV_is_IX);
-  tcase_add_test(addition, IX_plus_I_is_X);
-
-  return addition;
 }
 
 static TCase *subtraction(void) {
