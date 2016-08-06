40675448bf4eb85cf81a743c5e976e845aeb00cc
Drew Keller <drew@eightycolumns.com>
2016-08-06 18:45:33 -0400

    Add vertical whitespace to subtraction()

--- test/subtraction.c
+++ test/subtraction.c
@@ -122,24 +122,29 @@ START_TEST(mmmcccxxxiii_minus_mmccxxii_is_MCXI) {
 
 TCase *subtraction(void) {
   TCase *subtraction = tcase_create("Subtraction");
+
   tcase_add_test(subtraction, MMMCMXCIX_minus_CXI_is_MMMDCCCLXXXVIII);
   tcase_add_test(subtraction, MMMDCCCLXXXVIII_minus_DCCCLXXXVIII_is_MMM);
   tcase_add_test(subtraction, MMM_minus_MM_is_M);
+
   tcase_add_test(subtraction, M_minus_C_is_CM);
   tcase_add_test(subtraction, CM_minus_CD_is_D);
   tcase_add_test(subtraction, D_minus_C_is_CD);
   tcase_add_test(subtraction, CD_minus_CC_is_CC);
   tcase_add_test(subtraction, CC_minus_C_is_C);
+
   tcase_add_test(subtraction, C_minus_X_is_XC);
   tcase_add_test(subtraction, XC_minus_XL_is_L);
   tcase_add_test(subtraction, L_minus_X_is_XL);
   tcase_add_test(subtraction, XL_minus_XX_is_XX);
   tcase_add_test(subtraction, XX_minus_X_is_X);
+
   tcase_add_test(subtraction, X_minus_I_is_IX);
   tcase_add_test(subtraction, IX_minus_IV_is_V);
   tcase_add_test(subtraction, V_minus_I_is_IV);
   tcase_add_test(subtraction, IV_minus_II_is_II);
   tcase_add_test(subtraction, II_minus_I_is_I);
+
   tcase_add_test(subtraction, mmmcccxxxiii_minus_mmccxxii_is_MCXI);
 
   return subtraction;
