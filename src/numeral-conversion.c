c4e14635066f757df5bb4cd1b67ca19373ad2f4b
Drew Keller <drew@eightycolumns.com>
2016-08-06 13:57:14 -0400

    Rework roman_to_arabic()
    Rework roman_to_arabic() to use the same numerals array as
    arabic_to_roman().

--- src/numeral-conversion.c
+++ src/numeral-conversion.c
@@ -1,6 +1,7 @@
 #include "src/numeral-conversion.h"
 
 #include <assert.h>
+#include <stdbool.h>
 #include <string.h>
 
 typedef struct {
@@ -9,13 +10,11 @@ typedef struct {
 } Numeral;
 
 static Numeral numerals[] = {
-  {"I", 1},
-  {"II", 2},
-  {"III", 3},
-  {"IV", 4},
-  {"V", 5},
+  {"X", 10},
   {"IX", 9},
-  {"X", 10}
+  {"V", 5},
+  {"IV", 4},
+  {"I", 1}
 };
 
 static size_t numeral_count = sizeof numerals / sizeof numerals[0];
@@ -25,9 +24,41 @@ int roman_to_arabic(const char *roman) {
 
   int arabic = 0;
 
-  for (size_t i = 0; i < numeral_count; i += 1) {
-    if (strcmp(roman, numerals[i].key) == 0) {
-      arabic = numerals[i].value;
+  size_t i = 0;
+
+  while (i < strlen(roman)) {
+    char two_char_substring[3];
+    strncpy(two_char_substring, &roman[i], 2);
+    two_char_substring[2] = '\0';
+
+    char one_char_substring[2];
+    strncpy(one_char_substring, &roman[i], 1);
+    one_char_substring[1] = '\0';
+
+    bool numerals_array_includes_two_char_substring = false;
+
+    for (size_t j = 0; j < numeral_count; j += 1) {
+      if (strcmp(two_char_substring, numerals[j].key) == 0) {
+        numerals_array_includes_two_char_substring = true;
+      }
+    }
+
+    if (numerals_array_includes_two_char_substring) {
+      for (size_t k = 0; k < numeral_count; k += 1) {
+        if (strcmp(two_char_substring, numerals[k].key) == 0) {
+          arabic += numerals[k].value;
+        }
+      }
+
+      i += 2;
+    } else {
+      for (size_t k = 0; k < numeral_count; k += 1) {
+        if (strcmp(one_char_substring, numerals[k].key) == 0) {
+          arabic += numerals[k].value;
+        }
+      }
+
+      i += 1;
     }
   }
 
@@ -36,16 +67,6 @@ int roman_to_arabic(const char *roman) {
 
 char *arabic_to_roman(char *roman, int arabic) {
   assert(roman != NULL);
-
-  Numeral numerals[] = {
-    {"X", 10},
-    {"IX", 9},
-    {"V", 5},
-    {"IV", 4},
-    {"I", 1}
-  };
-
-  size_t numeral_count = sizeof numerals / sizeof numerals[0];
 
   strcpy(roman, "");
 
