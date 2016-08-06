88b256cd6ce6f841ecc2d8f12fd09354ef76073c
Drew Keller <drew@eightycolumns.com>
2016-08-06 15:28:56 -0400

    Reject XXL as an invalid operand

--- test/invalid-operand-error.c
+++ test/invalid-operand-error.c
@@ -48,6 +48,12 @@ START_TEST(LL_is_an_invalid_operand) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(XXL_is_an_invalid_operand) {
+  int expected = ROMCALC_INVALID_OPERAND_ERROR;
+  int actual = add(sum, "XXL", "I");
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *invalid_operand_error(void) {
   TCase *invalid_operand_error = tcase_create("Invalid Operand Error");
   tcase_add_test(invalid_operand_error, the_empty_string_is_an_invalid_operand);
@@ -57,6 +63,7 @@ TCase *invalid_operand_error(void) {
   tcase_add_test(invalid_operand_error, VX_is_an_invalid_operand);
   tcase_add_test(invalid_operand_error, XXXX_is_an_invalid_operand);
   tcase_add_test(invalid_operand_error, LL_is_an_invalid_operand);
+  tcase_add_test(invalid_operand_error, XXL_is_an_invalid_operand);
 
   return invalid_operand_error;
 }
