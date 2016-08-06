3e3c78060bbc589d9721d5790f9ba3cd88d700bf
Drew Keller <drew@eightycolumns.com>
2016-08-05 20:28:43 -0400

    Extract error_handling() from main()

--- test/romcalc-test.c
+++ test/romcalc-test.c
@@ -9,17 +9,14 @@
 #include "test/addition.h"
 #include "test/subtraction.h"
 
-int main(void) {
-  Suite *error_handling = suite_create("Error Handling");
-  suite_add_tcase(error_handling, success());
-  suite_add_tcase(error_handling, null_pointer_error());
-  suite_add_tcase(error_handling, invalid_operand_error());
+static Suite *error_handling(void);
 
+int main(void) {
   Suite *operations = suite_create("Operations");
   suite_add_tcase(operations, addition());
   suite_add_tcase(operations, subtraction());
 
-  SRunner *srunner = srunner_create(error_handling);
+  SRunner *srunner = srunner_create(error_handling());
   srunner_add_suite(srunner, operations);
 
   srunner_run_all(srunner, CK_ENV);
@@ -27,4 +24,13 @@ int main(void) {
   srunner_free(srunner);
 
   return (ntests_failed > 0) ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+static Suite *error_handling(void) {
+  Suite *error_handling = suite_create("Error Handling");
+  suite_add_tcase(error_handling, success());
+  suite_add_tcase(error_handling, null_pointer_error());
+  suite_add_tcase(error_handling, invalid_operand_error());
+
+  return error_handling;
 }
