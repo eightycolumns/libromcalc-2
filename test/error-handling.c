e0bc3f49490df29d6d758f3d227ed2d70bea9b7f
Drew Keller <drew@eightycolumns.com>
2016-08-06 18:38:54 -0400

    Add vertical whitespace to error_handling()

--- test/error-handling.c
+++ test/error-handling.c
@@ -9,6 +9,7 @@
 
 Suite *error_handling(void) {
   Suite *error_handling = suite_create("Error Handling");
+
   suite_add_tcase(error_handling, success());
   suite_add_tcase(error_handling, null_pointer_error());
   suite_add_tcase(error_handling, invalid_operand_error());
